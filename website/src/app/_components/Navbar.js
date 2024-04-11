2b4c57fb8ddb1991ed4934b4711affad86abe2e0
NikkoPacleb <pacleb.nikko@gmail.com>
2024-04-10 22:45:20 -0700

    Main Page added

---
+++ website/src/app/_components/Navbar.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="bg-background py-4 items-center flex justify-around container lg:my-4">
+      <div className="hidden lg:flex text-3xl w-3/4 font-bold">
+        <h1>SMACS-Mt San Antonio College</h1>
+      </div>
+      <div className="flex justify-around lg:justify-end w-full lg:space-x-12 ">
+        <Link
+          href="/"
+          className={`${
+            pathname === "/" ? "font-bold" : ""
+          } md:text-xl lg:text-2xl`}
+        >
+          Home
+        </Link>
+        <Link
+          href="/events"
+          className={`${
+            pathname === "/events" ? "font-bold" : ""
+          } md:text-xl lg:text-2xl`}
+        >
+          Events
+        </Link>
+        <Link
+          href="/projects"
+          className={`${
+            pathname === "/projects" ? "font-bold" : ""
+          } md:text-xl lg:text-2xl`}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/about"
+          className={`${
+            pathname === "/about" ? "font-bold" : ""
+          } md:text-xl lg:text-2xl`}
+        >
+          About
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
